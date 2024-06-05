3754820f996774ada9cfddbcf9c42d2c51072dad
deeztrzl <148042421+deeztrzl@users.noreply.github.com>
2024-06-05 23:15:12 +0700

    Update adminpage.php

--- haloPetani/adminpage.php
+++ haloPetani/adminpage.php
@@ -103,9 +103,9 @@ $foto_profil = "foto_profil.jpg";
         </header>
         <nav>
             <ul>
-                <li><a href="#">Dashboard</a></li>
                 <li><a href="artikel.php">Manajemen Artikel</a></li>
                 <li><a href="list-tanaman.php">Manajemen Pertanyaan dan Jawaban</a></li>
+                <li><a href="login-pengguna">LogOut</a></li>
             </ul>
         </nav>
         <div class="content">
